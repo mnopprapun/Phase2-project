c1226aa436ca41773dfd81defcdeaaff06f44335
Marc Nopprapun <44753875+mnopprapun@users.noreply.github.com>
2021-05-10 16:14:04 -0500

    Add files via upload

---
+++ src/CitySearch/CitySearch.js
@@ -0,0 +1,21 @@
+
+
+import React from 'react';
+import LiveCard from "./LiveCard";
+
+const CitySearch = () => {
+
+	return (
+
+		<div>
+			<form>
+				Input Field
+			</form>
+			<button>Test</button>
+			<LiveCard />
+		</div>
+	);
+}
+
+
+export default CitySearch;
---
+++ src/CitySearch/LiveCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
+
+
+
+class LiveCard extends Component {
+
+	render() {
+		return (
+			<MDBCard style={{ maxWidth: '22rem' }}>
+				<MDBCardBody>
+					<MDBCardTitle>Card title</MDBCardTitle>
+					<MDBCardText>
+						Some quick example text to build on the card title and make up the bulk of the card's content.
+        </MDBCardText>
+					<MDBBtn>Button</MDBBtn>
+				</MDBCardBody>
+			</MDBCard>
+		);
+	}
+}
+
+
+export default LiveCard;
