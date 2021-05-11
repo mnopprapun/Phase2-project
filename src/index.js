974488c1b650b10da24a1bc5d8cca0cc80190187
Marc Nopprapun <44753875+mnopprapun@users.noreply.github.com>
2021-05-11 15:42:43 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,15 +4,45 @@ import './index.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import About from './Routes/About'
+import MyCities from './Routes/MyCities'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <nav>
+        <Link to="/MyCity">My City</Link>
+      </nav>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <Switch>
+        <Route path="/">
+        </Route>
+        <Route path="/MyCity">
+          <MyCities />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route >
+      </Switch>
+
+    </Router>
+
     <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
