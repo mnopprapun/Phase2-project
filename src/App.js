adc68067495c1c8379ed7096d4ff42f84b14e2d2
Marc Nopprapun <44753875+mnopprapun@users.noreply.github.com>
2021-05-11 15:43:09 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import CardContainer from './CardContainer/CardContainer';
-import CitySearch from './CitySearch/CitySearch'
+import CitySearch from './CitySearch/CitySearch';
+
+
 
 const cityUrl = 'http://localhost:3000/cities'
 
-class App extends React.Component{
+class App extends React.Component {
 
 
-componentDidMount = () => {
-  fetch(cityUrl)
-  .then(res=>res.json())
-  .then(data=>console.log(data))
-}
+  componentDidMount = () => {
+    fetch(cityUrl)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
 
-render(){
-  return (
-    <div>
-      <CitySearch />
+  render() {
+    return (
       <div>
-        <CardContainer />
+        <div>
+          <CitySearch />
+        </div>
+        <div>
+          <CardContainer />
+        </div>
       </div>
-    </div>
-  );
+    );
   }
 }
 export default App
